7c3548cace16bad35bb568da178895b9f1688bee
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2018-06-06 16:04:58 +0300

    Add play button

--- app/src/main/java/com/yarmiychuk/udamusicplayer/activities/PlayerActivity.java
+++ app/src/main/java/com/yarmiychuk/udamusicplayer/activities/PlayerActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yarmiychuk.udamusicplayer.R;
 
@@ -17,7 +21,10 @@ public class PlayerActivity extends AppCompatActivity {
     public static final String INTENT_ARTIST = "intent_artist";
     public static final String INTENT_ALBUM = "intent_album";
 
+    private boolean isPlaying;
+
     private TextView tvTrackName, tvArtist, tvAlbum;
+    private Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class PlayerActivity extends AppCompatActivity {
         initializeViews();
 
         setCaptions();
+
+        isPlaying = false;
     }
 
     private void initializeViews() {
@@ -38,11 +47,30 @@ public class PlayerActivity extends AppCompatActivity {
         // Album cover
         ImageView ivAlbumCover = findViewById(R.id.iv_album_cover);
         int size = Resources.getSystem().getDisplayMetrics().widthPixels;
-        ivAlbumCover.setLayoutParams(new LinearLayout.LayoutParams(size, size));
+        ivAlbumCover.setLayoutParams(new RelativeLayout.LayoutParams(size, size));
         // Track info
         tvTrackName = findViewById(R.id.tv_track_name);
         tvArtist = findViewById(R.id.tv_artist_name);
         tvAlbum = findViewById(R.id.tv_album);
+        // Play/Stop button
+        btnPlay = findViewById(R.id.button_play);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    // Stop music
+                    btnPlay.setText(getString(R.string.play));
+                    isPlaying = false;
+                } else {
+                    // Play music
+                    btnPlay.setText(getString(R.string.stop));
+                    Toast.makeText(PlayerActivity.this,
+                            getString(R.string.music_is_playing),
+                            Toast.LENGTH_SHORT).show();
+                    isPlaying = true;
+                }
+            }
+        });
     }
 
     /**
